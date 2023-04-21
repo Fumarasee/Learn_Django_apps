1b84ed0faa04cd36d75d2b7d9fbaff0c4778e068
Tkachenko Mihail <algorithmpower@gmail.com>
2023-04-21 22:25:04 +0300

    typos correction

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,25 +9,25 @@ class Post(models.Model):
             PUBLISHED = 'PB', 'Published'
             
 
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,
+        title = models.CharField(max_length=250)
+        slug = models.SlugField(max_length=250)
+        body = models.TextField()
+        publish = models.DateTimeField(default=timezone.now)
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        status = models.CharField(max_length=2,
                                 choices=Status.choices,
                                 default=Status.DRAFT)
 
 
-    class Meta:
-        ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['publlish']),
-        ]
+        class Meta:
+            ordering = ['-publish']
+            indexes = [
+                models.Index(fields=['-publish']),
+            ]
 
-    def __str__(self):
-        return self.title
+        def __str__(self):
+            return self.title
 
 
 # Create your models here.

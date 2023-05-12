88afe1e2b96bb8b8540582fe408c51a09dd18c4d
Tkachenko Mihail <algorithmpower@gmail.com>
2023-05-12 10:52:34 +0300

    page exceptions added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage,\
+                                    PageNotAnInteger
 
 
 
@@ -11,7 +12,14 @@ def post_list(request):
     # pagination with 3 posts/page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)    
 
     return render(request,
                     'blog/post/list.html',
